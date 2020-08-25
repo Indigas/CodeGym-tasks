6c3409d88628573444b1e2b2835ab761d0fbce60
Indigo <durovic.marek@gmail.com>
2020-08-25 14:47:22 +0200

    updating task 2515

--- 4.JavaCollections/src/com/codegym/task/task25/task2515/Rocket.java
+++ 4.JavaCollections/src/com/codegym/task/task25/task2515/Rocket.java
@@ -2,7 +2,17 @@ package com.codegym.task.task25.task2515;
 
 public class Rocket extends BaseObject {
 
-    public Rocket(double x, double y, double radius) {
-        super(x, y, radius);
+    public Rocket(double x, double y) {
+        super(x, y, 1);
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        canvas.setPoint(this.x, this.y, 'R');
+    }
+
+    @Override
+    public void move() {
+        this.y--;
     }
 }
