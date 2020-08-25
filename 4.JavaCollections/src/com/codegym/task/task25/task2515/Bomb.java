3914119800e9d68a6e64097b9f39e401c9fed57b
Indigo <durovic.marek@gmail.com>
2020-08-25 14:44:23 +0200

    updating task 2515

--- 4.JavaCollections/src/com/codegym/task/task25/task2515/Bomb.java
+++ 4.JavaCollections/src/com/codegym/task/task25/task2515/Bomb.java
@@ -2,8 +2,18 @@ package com.codegym.task.task25.task2515;
 
 public class Bomb extends BaseObject{
 
-    public Bomb(double x, double y, double radius) {
-        super(x, y, radius);
+    public Bomb(double x, double y) {
+        super(x, y, 1);
+    }
+
+    @Override
+    public void move() {
+        this.y++;
+    }
+
+    @Override
+    public void draw(Canvas canvas){
+        canvas.setPoint(this.x, this.y, 'B');
     }
 
 
