341e1500dd040c624b6ff40bfb21d1b7f3466f4f
Indigo <durovic.marek@gmail.com>
2020-08-25 14:49:28 +0200

    updating task 2515

--- 4.JavaCollections/src/com/codegym/task/task25/task2515/Rocket.java
+++ 4.JavaCollections/src/com/codegym/task/task25/task2515/Rocket.java
@@ -1,18 +1,27 @@
 package com.codegym.task.task25.task2515;
 
+/**
+ * Rocket class
+ */
 public class Rocket extends BaseObject {
 
     public Rocket(double x, double y) {
         super(x, y, 1);
     }
 
+    /**
+     * This method draws the object on the canvas.
+     */
     @Override
     public void draw(Canvas canvas) {
-        canvas.setPoint(this.x, this.y, 'R');
+        canvas.setPoint(x, y, 'R');
     }
 
+    /**
+     * Move the object up by one step.
+     */
     @Override
     public void move() {
-        this.y--;
+        y--;
     }
 }
--- 4.JavaCollections/src/com/codegym/task/task25/task2515/Spaceship.java
+++ 4.JavaCollections/src/com/codegym/task/task25/task2515/Spaceship.java
@@ -1,8 +1,17 @@
 package com.codegym.task.task25.task2515;
 
 public class Spaceship extends BaseObject {
+    private double dx = 0d;
 
-    public Spaceship(double x, double y, double radius) {
-        super(x, y, radius);
+    public Spaceship(double x, double y) {
+        super(x, y, 3);
+    }
+
+    public void moveLeft(){
+        this.dx = -1;
+    }
+
+    public void moveRight(){
+        this.dx = 1;
     }
 }
