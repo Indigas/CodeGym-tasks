a0fd4800434d44452b67fff22890df1017d16652
Indigo <durovic.marek@gmail.com>
2020-09-03 18:14:09 +0200

    continue task 3310

---
+++ 4.JavaCollections/src/com/codegym/task/task33/task3310/Shortener.java
@@ -0,0 +1,17 @@
+package com.codegym.task.task33.task3310;
+
+import java.util.HashMap;
+
+public class Shortener {
+    private long id;
+    private String string;
+
+
+    public Long getId(String string) {
+        return 0l;
+    }
+
+    public String getString(Long id) {
+        return string;
+    }
+}
