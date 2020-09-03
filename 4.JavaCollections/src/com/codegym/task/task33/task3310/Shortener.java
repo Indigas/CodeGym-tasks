6b328a4ee563b56d6df5897ffb7e490338b65a5d
Indigo <durovic.marek@gmail.com>
2020-09-03 18:41:18 +0200

    continue task 3310 - 4 part

--- 4.JavaCollections/src/com/codegym/task/task33/task3310/Shortener.java
+++ 4.JavaCollections/src/com/codegym/task/task33/task3310/Shortener.java
@@ -1,17 +1,29 @@
 package com.codegym.task.task33.task3310;
 
+import com.codegym.task.task33.task3310.strategy.StorageStrategy;
+
 import java.util.HashMap;
 
 public class Shortener {
-    private long id;
+    private Long lastId=0L;
     private String string;
+    private StorageStrategy storageStrategy;
 
+    public Shortener(StorageStrategy storageStrategy) {
+        this.storageStrategy = storageStrategy;
+    }
 
-    public Long getId(String string) {
-        return 0l;
+    public synchronized Long getId(String string) {
+        if(storageStrategy.containsValue(string))
+            return storageStrategy.getKey(string);
+
+        this.lastId++;
+        storageStrategy.put(this.lastId, string);
+
+        return this.lastId;
     }
 
     public String getString(Long id) {
-        return string;
+        return storageStrategy.getValue(id);
     }
 }
