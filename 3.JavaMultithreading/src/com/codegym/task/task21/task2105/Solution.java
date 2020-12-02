dca40f6954b5e4b2923b7c86d19f1fa8374e1aee
Indigo <durovic.marek@gmail.com>
2020-12-02 19:44:26 +0100

    finished multiple tasks

---
+++ 3.JavaMultithreading/src/com/codegym/task/task21/task2105/Solution.java
@@ -0,0 +1,42 @@
+package com.codegym.task.task21.task2105;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/* 
+Fix the bug. Comparing objects
+
+*/
+
+public class Solution {
+    private final String first, last;
+
+    public Solution(String first, String last) {
+        this.first = first;
+        this.last = last;
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Solution)) return false;
+
+        Solution solution = (Solution) o;
+
+        if (first != null ? !first.equals(solution.first) : solution.first != null) return false;
+        if (last != null ? !last.equals(solution.last) : solution.last != null) return false;
+
+        return true;
+    }
+
+
+    @Override
+    public int hashCode() {
+        return (first+last).hashCode();
+    }
+
+    public static void main(String[] args) {
+        Set<Solution> s = new HashSet<>();
+        s.add(new Solution("Mickey", "Mouse"));
+        System.out.println(s.contains(new Solution("Mickey", "Mouse")));
+    }
+}
